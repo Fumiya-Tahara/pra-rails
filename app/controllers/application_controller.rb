caade8ba8c2ed3741f7b361a7d7c90f58f131e76
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-12-07 15:33:34 +0000

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :basic_auth
+
+  private
+    def basic_auth
+      authenticate_or_request_with_http_basic("Application") do |user_name, password|
+        user = User.find_by(username: user_name)
+        user&.authenticate(password)
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_03_121427) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
