8df3606ac6319a9899f3ad9f0ca5ab3d967a5b13
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-12-08 09:31:36 +0000

    Add post creation process and post list process

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to action: "index"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.expect(post: [ :content ])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   root "posts#index"
 
   resources :posts
+  get "/post", to: "posts#new"
 end
